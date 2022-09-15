053d8341920f635960ab44ca060ebfb440aecf9b
zhenyue <zhenyuef@gmail.com>
2022-09-15 18:12:04 +0200

    不同组的参数

--- api/gen.py
+++ api/gen.py
@@ -1,64 +1,50 @@
 from http.server import BaseHTTPRequestHandler
+from itertools import count
 
 from os.path import join
 import re
 from datetime import datetime
-from urllib.parse import parse_qs, urlparse
+from urllib.parse import parse_qs, parse_qsl, urlparse
 
-from ics import Calendar
+from ics import Calendar, Container, ContentLine
 
-cal = dict()
+CALENDARS: dict[str, Calendar] = dict()
 start_date = datetime.fromisoformat("2022-09-04T12:05:23+02:00")
 parcours = {"MOGPL": "AND", "IL": "STL", "LRC": "DAC", "MLBDA": "DAC", "MAPSI": "IMA"}
 
 
 def load_calendar(name: str):
     with open(join("data", name + ".ics"), "r") as f:
-        cal[name] = Calendar(f.read())
+        CALENDARS[name] = Calendar(f.read())
 
 
-def filter_cours(ue: dict, majour: str):
+def filter_ue_parcours(ue: dict):
+    parcours_ue: dict[str, list[str]] = dict()
+    for key in ue.keys():
+        parcours_ue[key].append(parcours[key])
+    return parcours_ue
+
+
+def filter_cours(ue_group: dict, majour: str):
     cal = Calendar()
-    iter_and = AND.timeline.start_after(start_date)
-    for event in iter_and:
-        if MOGPL != 0 and "MOGPL" in event.summary:
-            pattern = "T\w{1,2}(?=\d)(?!" + str(MOGPL) + ")"
-            if re.search(pattern, event.summary):
-                continue
-            cal.events.append(event)
-        if "AND" in event.summary:
-            cal.events.append(event)
-
-    iter_stl = STL.timeline.start_after(start_date)
-    for event in iter_stl:
-        if IL != 0 and "IL" in event.summary:
-            pattern = "T\w{1,2}(?=\d)(?!" + str(IL) + ")"
-            if re.search(pattern, event.summary):
-                continue
-            cal.events.append(event)
-
-    iter_dac = DAC.timeline.start_after(start_date)
-    for event in iter_dac:
-        if LRC != 0 and "LRC" in event.summary:
-            pattern = "T\w{1,2}(?=\d)(?!" + str(LRC) + ")"
-            if re.search(pattern, event.summary, 0):
-                continue
-            cal.events.append(event)
-
-        if MLBDA != 0 and "MLBDA" in event.summary:
-            pattern = "T\w{1,2}(?=\d)(?!" + str(MLBDA) + ")"
-            if re.search(pattern, event.summary, 0):
-                continue
-            cal.events.append(event)
-
-    iter_ima = IMA.timeline.start_after(start_date)
-    for event in iter_ima:
-        if MAPSI != 0 and "MAPSI" in event.summary:
-            pattern = "T\w{1,2}(?=\d)(?!" + str(MAPSI) + ")"
-            if re.search(pattern, event.summary, 0):
-                continue
-            cal.events.append(event)
-
+    cal.extra = Container(
+        "VCALENDAR", [ContentLine("X-WR-CALNAME", value="M1 " + majour)]
+    )
+    parcours_ue = filter_ue_parcours(ue_group)
+    for parcour in parcours_ue.keys():
+        load_calendar(parcour)
+        iter_parcour = CALENDARS[parcour].timeline.start_after(start_date)
+        for event in iter_parcour:
+            if majour == parcour:
+                if parcour in event.summary:
+                    cal.events.append(event)
+                    continue
+            for ue in parcours_ue[parcour]:
+                if ue in event.summary:
+                    pattern = "T\w{1,2}(?=\d)(?!" + str(ue_group[ue]) + ")"
+                    if not re.search(pattern, event.summary):
+                        cal.events.append(event)
+                        break
     return cal
 
 
@@ -69,7 +55,9 @@ class handler(BaseHTTPRequestHandler):
         self.send_header("Cache-Control", "s-maxage=259200")
         self.end_headers()
         url = urlparse(self.path)
-        query = parse_qs(url.query)
-        cal = filter_cours(3, 2, 1, 1, 3)
+        query = parse_qsl(url.query)
+        ues = dict(query)
+        major = ues.pop("MAJ")
+        cal = filter_cours(ues, major)
         self.wfile.write(cal.serialize().encode())
         return
