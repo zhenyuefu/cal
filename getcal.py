233ee55ca1e4d8d4c0df38e6637d6279ec92e79a
zhenyue <zhenyuef@gmail.com>
2022-09-20 15:12:50 +0200

    修复complex td1

--- getcal.py
+++ getcal.py
@@ -37,8 +37,25 @@ def save():
 
 start_date = date.fromisoformat("2022-09-04")
 
+def fix_SFPN():
+    for event in cal["SFPN"].timeline.__iter__():
+        if "MU4IN900-COMPLEX-TD" in event.summary:
+            event.summary = "MU4IN900-COMPLEX-TD1"
+            continue
+        if "MU4IN900-COMPLEX-TME" in event.summary:
+            event.summary = "MU4IN900-COMPLEX-TME1"
+            continue
+        if "MU4IN901-MODEL-TD" in event.summary:
+            event.summary = "MU4IN901-MODEL-TD1"
+            continue
+        if "MU4IN901-MODEL-TME" in event.summary:
+            event.summary = "MU4IN901-MODEL-TME1"
+            continue
+
 
 for name in url.keys():
     get_remote(name)
     remove_events_before(cal[name], start_date)
+    if name == "SFPN":
+        fix_SFPN()
     save()
